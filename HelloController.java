30e08a3262b55cb5ecaa4d11c1ce8265b481e69f
nitintg <nitintg94@gmail.com>
2015-09-18 12:21:02 +0530

    Initial commits
    Initial commits

---
+++ HelloController.java
@@ -0,0 +1,14 @@
+package com.gontuseries.hellocontroller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+@Controller
+public class HelloController {
+	@RequestMapping("/welcome")
+	public ModelAndView helloWorld() {
+		ModelAndView model = new ModelAndView("HelloPage");
+		model.addObject("msg","Hello World");
+		return model;
+	}
+}
+
---
+++ HelloPage.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+	<h1>First Spring MVC Application Demo</h1>
+	<h2>${msg}</h2>
+</body>
+</html>
